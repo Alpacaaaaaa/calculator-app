27a0104d7a4f23e24e2ea3a1b970b5bc6c531b78
wang-hr20 <wang_hr20@163.com>
2022-02-17 00:03:25 +0800

    尝试加入符号运算，实现包含pi和e的四则运算，增加工具栏设置选项

---
+++ SETTINGS.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QRadioButton, QApplication, QPushButton, QMessageBox, QButtonGroup, QGridLayout, QLabel
+
+class SETTINGS(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        self.OUTPUT_FORMAT=QLabel("输出形式：")
+        self.math_output = QRadioButton('数学输出',self)
+        self.digit_output = QRadioButton('小数输出',self)
+        self.digit_output.setChecked(True)
+        self.output_format = QButtonGroup(self)
+        self.output_format.addButton(self.math_output, 11)
+        self.output_format.addButton(self.digit_output, 12)
+        grid.addWidget(self.OUTPUT_FORMAT,0,0)
+        grid.addWidget(self.math_output,0,1)
+        grid.addWidget(self.digit_output,0,2)
+        self.digit_output.toggled.connect(self.output_toggled)
+
+        self.ANGLE=QLabel("角度单位：")
+        self.rad = QRadioButton('弧度',self)
+        self.rad.setChecked(True)
+        self.deg = QRadioButton('角度',self)
+        self.angle = QButtonGroup(self)
+        self.angle.addButton(self.rad, 21)
+        self.angle.addButton(self.deg, 22)
+        grid.addWidget(self.ANGLE,1,0)
+        grid.addWidget(self.rad,1,1)
+        grid.addWidget(self.deg,1,2)
+        self.rad.toggled.connect(self.angle_toggled)
+
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('设置')    
+        self.show()
+
+        self.sym=False
+        self.ang="rad"
+    
+    def output_toggled(self):
+            self.sym=False if self.digit_output.isChecked() else True
+            # print(self.sym)
+
+    def angle_toggled(self):
+            self.ang="rad" if self.rad.isChecked() else "deg"
+            # print(self.ang)
--- calculator.py
+++ calculator.py
@@ -1,15 +1,21 @@
+'''
+尝试加入符号运算.
+目前实现的是包含pi和e的四则运算的符号化，并且在工具栏提供了设置选项
+'''
 import numpy as np
 import sys
-from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QApplication, QLabel
+import sympy
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QApplication, QLabel, QAction, QMainWindow
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QIcon
+from SETTINGS import SETTINGS
 
 class expression(): #表达式类，用来计算一个形如(a ± b */ c)的表达式的值。
     def __init__(self):
         self.res=0  #当前表达式中已经计算完了的部分，对应上面的a
         self.prev_sym=""    #对应a、b之间的那个±.表示正在运算的部分应当被加到res中还是从res中减去.
-        self.prev_num=0     #对应b，被乘/除数
-        self.curr_num=0     #对应c，乘/除数
+        self.prev_num=None     #对应b，被乘/除数
+        self.curr_num=None  #对应c，乘/除数
         self.curr_sym=""    #对应b、c之间的*/.表示b、c相乘还是相除
         self.curr_num_text=""
 #利用栈结构来处理括号嵌套.每遇到'('就把当前的expression对象入栈，在栈顶新开一个expression对象处理当前的括号.每遇到')'就出栈，处理上一级括号
@@ -22,8 +28,11 @@ CAL.append(EXP)
 def ERROR_INPUT():
     return None
 
+
+
+
 #GUI界面
-class Example(QWidget):
+class Calculator(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -31,14 +40,30 @@ class Example(QWidget):
         self.restart=True   #restart标志量表示是否重开
 
     def initUI(self):
-        
         grid = QGridLayout()
-        self.setLayout(grid)
 
-        self.names = ['Cls', 'Bck', '(', ')', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+']
+        settings = QAction('设置', self)
+        # settings.setShortcut('Ctrl+Q')
+        settings.triggered.connect(self.show_option)
+
+        self.toolbar = self.addToolBar('toolbar')
+        self.toolbar.addAction(settings)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Calculator Simple')    
+        self.show()
+
+        self.setting_dialog=SETTINGS()
+
+
+        self.names = ['ln', 'Cls', 'Bck', '(', ')', 'exp','7', '8', '9', '/', 'sqrt', '4', '5', '6', '*','e', '1', '2', '3', '-', 'pi', '0', '.', '=', '+']
         self.operators = ['(', ')', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '+', '=']
+        self.sym_const = {'e':sympy.E, 'pi':sympy.pi}
+        self.num_const = {'e':2.718281828459, 'pi':3.141592653589}
+        self.constants = self.sym_const if self.setting_dialog.sym else self.num_const
+        self.functions = ['ln', 'exp', 'sqrt']
 
-        positions = [(i+20,j) for i in range(5) for j in range(4)]
+        positions = [(i+20,j) for i in range(5) for j in range(5)]
         
         for position, name in zip(positions, self.names):   #对每个button设置位置、文字与快捷键
             if name == '':
@@ -53,13 +78,17 @@ class Example(QWidget):
         #用于显示输入表达式的label
         self.exp=""
         self.label_exp = QLabel(self.exp, self)
-        grid.addWidget(self.label_exp, 0, 0, 1, 4)
+        grid.addWidget(self.label_exp, 0, 0, 1, 5)
         self.label_exp.setAlignment(Qt.AlignRight)
 
         #用于显示计算结果的label
         self.label_ans = QLabel(self)
-        grid.addWidget(self.label_ans, 10, 0, 1, 4)
+        grid.addWidget(self.label_ans, 10, 0, 1, 5)
         self.label_ans.setAlignment(Qt.AlignRight)
+
+        widget = QWidget()
+        widget.setLayout(grid)
+        self.setCentralWidget(widget)
 
         self.move(300, 150)
         self.setWindowTitle('Calculator')
@@ -68,6 +97,7 @@ class Example(QWidget):
     #button被按下事件处理函数
     def INPUT(self):
         global CAL
+        self.constants = self.sym_const if self.setting_dialog.sym else self.num_const
         if self.restart:    #如果需要重开，则清除CAL栈
             CAL.clear()
             EXP=expression()
@@ -81,7 +111,13 @@ class Example(QWidget):
             self.label_exp.setText("")
             self.label_ans.setText("")
 
+        elif sender in self.constants.keys():
+            CAL[-1].curr_num=self.constants[sender]
+            self.exp+=sender
+
+
         elif sender in self.operators:  #如果按下的是运算符
+            
             self.exp=self.exp+sender
             self.restart=False
 
@@ -93,7 +129,8 @@ class Example(QWidget):
                 CAL[-1].curr_num_text+=sender
 
             elif (sender=="+" or sender=="-" or sender==")" or sender=="="):    #遇到+-)=，进行计算
-                if CAL[-1].curr_num_text!="":
+                # print(CAL[-1].curr_num)
+                if (CAL[-1].curr_num==None and CAL[-1].curr_num_text!=""):
                     CAL[-1].curr_num=float(CAL[-1].curr_num_text)
                 sgn=-1 if (CAL[-1].prev_sym=="-") else 1
                 if (CAL[-1].curr_sym==""):
@@ -107,7 +144,7 @@ class Example(QWidget):
 
                 CAL[-1].prev_sym=sender
                 CAL[-1].curr_sym=""
-                CAL[-1].prev_num=CAL[-1].curr_num=0
+                CAL[-1].prev_num=CAL[-1].curr_num=None
                 CAL[-1].curr_num_text=""
 
                 if sender == "=":
@@ -120,7 +157,7 @@ class Example(QWidget):
             elif (sender=="*" or sender=="/"):  #遇到*/，记录在prev_sym和prev_num中
                 if CAL[-1].curr_num_text!="":
                     CAL[-1].curr_num=float(CAL[-1].curr_num_text)
-                if CAL[-1].prev_num==0:
+                if CAL[-1].prev_num==None:
                     CAL[-1].prev_num=CAL[-1].curr_num
 
                 else:
@@ -129,17 +166,21 @@ class Example(QWidget):
                     elif (CAL[-1].curr_sym=="/"):
                         CAL[-1].prev_num=CAL[-1].prev_num/CAL[-1].curr_num
                 CAL[-1].curr_sym=sender
-                CAL[-1].curr_num=0
+                CAL[-1].curr_num=None
                 CAL[-1].curr_num_text=""
             
-            #输出结果与格式控制
-            self.label_exp.setFont(QFont("Roman Times", *(12,50) if self.restart else (16,75)))
-            self.label_exp.setText(self.exp)
-            self.label_ans.setFont(QFont("Roman Times", *(16,75) if self.restart else (12,50)))
-            self.label_ans.setText("{0}".format(CAL[0].res))
-            
+        #输出结果与格式控制
+        self.label_exp.setFont(QFont("Roman Times", *(12,50) if self.restart else (16,75)))
+        self.label_exp.setText(self.exp)
+        self.label_ans.setFont(QFont("Roman Times", *(16,75) if self.restart else (12,50)))
+        self.label_ans.setText("{0}".format(CAL[0].res))
+    
+    def show_option(self):
+        self.setting_dialog.show()
+        
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Calculator()
     sys.exit(app.exec_())
