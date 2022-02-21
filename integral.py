ab4a96d0095e94c14b83b59d96755273f5962df4
wang-hr20 <wang_hr20@163.com>
2022-02-21 11:51:06 +0800

    优化无穷积分，优化图片显示

--- integral.py
+++ integral.py
@@ -1,15 +1,16 @@
 '''
 输入定积分弹窗的实现
-现存问题：对不合法表达式抛异常；三重积分积分顺序；界面太丑了！！！svg图大小
+现存问题：对不合法表达式抛异常；三重积分积分顺序；界面太丑了！！！
+最近优化：无穷积分解析解,svg图大小
 '''
 import sys
-from PyQt5.QtWidgets import (QWidget, QLabel, QVBoxLayout, QMenu, QAction, QLineEdit, QTextEdit, QGridLayout, QApplication, QStackedWidget, QPushButton, QMainWindow)
+from PyQt5.QtWidgets import (QWidget, QLabel, QVBoxLayout, QMenu, QAction, QLineEdit, QTextEdit, QGridLayout, QStackedWidget, QPushButton, QMainWindow)
 from PyQt5 import QtSvg, QtCore
 import sympy
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtSvg import QSvgWidget
 from str2svg import tex2svg
-
+inf = {'+infty':+sympy.oo, '-infty':-sympy.oo, 'infty':sympy.oo}
 class IntFrame(QMainWindow):
     Signal = QtCore.pyqtSignal(str)
     def __init__(self, *args, **kwargs):
@@ -192,21 +193,40 @@ class IntFrame(QMainWindow):
 
     def Render(self):
         if self.stackedWidget.currentIndex() == 0:
-            upperbound = sympy.latex(sympy.sympify(self.uboundEdit.text())) if self.uboundEdit.text().strip() else ''
-            lowerbound = sympy.latex(sympy.sympify(self.lboundEdit.text())) if self.lboundEdit.text().strip() else ''
+            if self.uboundEdit.text() in inf:
+                upperbound = sympy.latex(inf[self.uboundEdit.text()])
+            else:
+                upperbound = sympy.latex(sympy.sympify(self.uboundEdit.text())) if self.uboundEdit.text().strip() else ''
+            if self.lboundEdit.text() in inf:
+                lowerbound = sympy.latex(inf[self.lboundEdit.text()])
+            else:
+                lowerbound = sympy.latex(sympy.sympify(self.lboundEdit.text())) if self.lboundEdit.text().strip() else ''
             f = sympy.latex(sympy.sympify(self.funcEdit.toPlainText())) if self.funcEdit.toPlainText().strip() else ''
             latex_code = r'\int_{'+lowerbound+'}^{'+upperbound+'}'+f+'\mathrm{d}x'
             self.svgWidget1.load(tex2svg(latex_code,1))
-            self.grid1.addWidget(self.svgWidget1,4,0,5,2)
+            self.grid1.addWidget(self.svgWidget1,4,0,5,2,QtCore.Qt.AlignCenter)
             self.svgWidget1.show()
             self.svgWidget1.setFixedHeight(40)
+            self.svgWidget1.setFixedWidth(80)
 
         elif self.stackedWidget.currentIndex() == 1:
-            upperbound = sympy.latex(sympy.sympify(self.uboundEdit21.text())) if self.uboundEdit21.text().strip() else ''
-            lowerbound = sympy.latex(sympy.sympify(self.lboundEdit21.text())) if self.lboundEdit21.text().strip() else ''
+            if self.uboundEdit21.text() in inf:
+                upperbound = sympy.latex(inf[self.uboundEdit21.text()])
+            else:
+                upperbound = sympy.latex(sympy.sympify(self.uboundEdit21.text())) if self.uboundEdit21.text().strip() else ''
+            if self.lboundEdit21.text() in inf:
+                lowerbound = sympy.latex(inf[self.lboundEdit21.text()])
+            else:
+                lowerbound = sympy.latex(sympy.sympify(self.lboundEdit21.text())) if self.lboundEdit21.text().strip() else ''
             f = sympy.latex(sympy.sympify(self.funcEdit2.toPlainText())) if self.funcEdit2.toPlainText().strip() else ''
-            upperbound2 = sympy.latex(sympy.sympify(self.uboundEdit22.text())) if self.uboundEdit22.text().strip() else ''
-            lowerbound2 = sympy.latex(sympy.sympify(self.lboundEdit22.text())) if self.lboundEdit22.text().strip() else ''
+            if self.uboundEdit22.text() in inf:
+                upperbound2 = sympy.latex(inf[self.uboundEdit22.text()])
+            else:
+                upperbound2 = sympy.latex(sympy.sympify(self.uboundEdit22.text())) if self.uboundEdit22.text().strip() else ''
+            if self.lboundEdit.text() in inf:
+                lowerbound2 = sympy.latex(inf[self.lboundEdit22.text()])
+            else:
+                lowerbound2 = sympy.latex(sympy.sympify(self.lboundEdit22.text())) if self.lboundEdit22.text().strip() else ''
             latex_code = r'\iint_{'+lowerbound+'\leq x\leq '+upperbound+';'+lowerbound2+'\leq y\leq '+upperbound2+'}'+f+'\mathrm{d}x\mathrm{d}y'
             self.svgWidget2.load(tex2svg(latex_code,1))
             self.grid2.addWidget(self.svgWidget2,6,0,5,2)
@@ -214,47 +234,142 @@ class IntFrame(QMainWindow):
             self.svgWidget2.setFixedHeight(40)
 
         else:
-            upperbound = sympy.latex(sympy.sympify(self.uboundEdit31.text())) if self.uboundEdit31.text().strip() else ''
-            lowerbound = sympy.latex(sympy.sympify(self.lboundEdit31.text())) if self.lboundEdit31.text().strip() else ''
+            if self.uboundEdit31.text() in inf:
+                upperbound = sympy.latex(inf[self.uboundEdit31.text()])
+            else:
+                upperbound = sympy.latex(sympy.sympify(self.uboundEdit31.text())) if self.uboundEdit31.text().strip() else ''
+            if self.lboundEdit31.text() in inf:
+                lowerbound = sympy.latex(inf[self.lboundEdit31.text()])
+            else:
+                lowerbound = sympy.latex(sympy.sympify(self.lboundEdit31.text())) if self.lboundEdit31.text().strip() else ''
+            
             f = sympy.latex(sympy.sympify(self.funcEdit3.toPlainText())) if self.funcEdit3.toPlainText().strip() else ''
-            upperbound2 = sympy.latex(sympy.sympify(self.uboundEdit32.text())) if self.uboundEdit32.text().strip() else ''
-            lowerbound2 = sympy.latex(sympy.sympify(self.lboundEdit32.text())) if self.lboundEdit32.text().strip() else ''
-            upperbound3 = sympy.latex(sympy.sympify(self.uboundEdit33.text())) if self.uboundEdit33.text().strip() else ''
-            lowerbound3 = sympy.latex(sympy.sympify(self.lboundEdit33.text())) if self.lboundEdit33.text().strip() else ''
+
+            if self.uboundEdit32.text() in inf:
+                upperbound2 = sympy.latex(inf[self.uboundEdit32.text()])
+            else:
+                upperbound2 = sympy.latex(sympy.sympify(self.uboundEdit32.text())) if self.uboundEdit32.text().strip() else ''
+            if self.lboundEdit.text() in inf:
+                lowerbound2 = sympy.latex(inf[self.lboundEdit32.text()])
+            else:
+                lowerbound2 = sympy.latex(sympy.sympify(self.lboundEdit32.text())) if self.lboundEdit32.text().strip() else ''
+            
+            if self.uboundEdit33.text() in inf:
+                upperbound3 = sympy.latex(inf[self.uboundEdit33.text()])
+            else:
+                upperbound3 = sympy.latex(sympy.sympify(self.uboundEdit33.text())) if self.uboundEdit33.text().strip() else ''
+            if self.lboundEdit.text() in inf:
+                lowerbound3 = sympy.latex(inf[self.lboundEdit33.text()])
+            else:
+                lowerbound3 = sympy.latex(sympy.sympify(self.lboundEdit33.text())) if self.lboundEdit33.text().strip() else ''
+
             latex_code = r'\iiint_{'+lowerbound+'\leq x\leq '+upperbound+';'+lowerbound2+'\leq y\leq '+upperbound2+';'+lowerbound3+'\leq z\leq '+upperbound3+'}'+f+'\mathrm{d}x\mathrm{d}y\mathrm{d}z'
             self.svgWidget3.load(tex2svg(latex_code,1))
             self.grid3.addWidget(self.svgWidget3,8,0,5,2)
             self.svgWidget3.show()
-            self.svgWidget3.setFixedHeight(40)
+            self.svgWidget3.setFixedHeight(35)
     
     def Conf(self):
         if self.stackedWidget.currentIndex()==0:
-            upperbound = sympy.sympify(self.uboundEdit.text()) if self.uboundEdit.text().strip() else ''
-            lowerbound = sympy.sympify(self.lboundEdit.text()) if self.lboundEdit.text().strip() else ''
-            f = sympy.sympify(self.funcEdit.toPlainText()) if self.funcEdit.toPlainText().strip() else ''
+            if self.uboundEdit.text() in inf:
+                upperbound = inf[self.uboundEdit.text()]
+            else:
+                upperbound = sympy.sympify(self.uboundEdit.text()) if self.uboundEdit.text().strip() else ''
+            if self.lboundEdit.text() in inf:
+                lowerbound = inf[self.lboundEdit.text()]
+            else:
+                lowerbound = sympy.sympify(self.lboundEdit.text()) if self.lboundEdit.text().strip() else ''
+            
+            f_text = self.funcEdit.toPlainText().replace('e^(','exp(')  #这里的目的是把'e^'全部用'exp'代替，否则可能有些无穷积分无法得到解析解
+            while f_text.find('e^')!=-1:
+                index = f_text.find('e^') + 2
+                while f_text[index] not in ['+','-','*','/','^',')']:
+                    if index+1 == len(f_text):
+                        break
+                    index += 1
+                f_text = f_text[:index+1] + ')' + f_text[index+1:]
+                f_text = f_text.replace('e^','exp(')
+            f = sympy.sympify(f_text) if self.funcEdit.toPlainText().strip() else ''
+            
             self.latex_code=r'\int_{'+sympy.latex(lowerbound)+'}^{'+sympy.latex(upperbound)+'}'+sympy.latex(f)+'\mathrm{d}x'
             x = sympy.Symbol('x')
             self.ans=sympy.integrate(f, (x, lowerbound, upperbound))
         elif self.stackedWidget.currentIndex()==1:
-            exchange = 'y' in self.uboundEdit21.text() or 'y' in self.lboundEdit21.text()
-            upperbound = sympy.sympify(self.uboundEdit21.text()) if self.uboundEdit21.text().strip() else ''
-            lowerbound = sympy.sympify(self.lboundEdit21.text()) if self.lboundEdit21.text().strip() else ''
-            f = sympy.sympify(self.funcEdit2.toPlainText()) if self.funcEdit2.toPlainText().strip() else ''
-            upperbound2 = sympy.sympify(self.uboundEdit22.text()) if self.uboundEdit22.text().strip() else ''
-            lowerbound2 = sympy.sympify(self.lboundEdit22.text()) if self.lboundEdit22.text().strip() else ''
+            exchange = not ('x' in self.uboundEdit22.text() + self.lboundEdit22.text())
+
+            if self.uboundEdit21.text() in inf:
+                upperbound = inf[self.uboundEdit21.text()]
+            else:
+                upperbound = sympy.sympify(self.uboundEdit21.text()) if self.uboundEdit21.text().strip() else ''
+            if self.lboundEdit21.text() in inf:
+                lowerbound = inf[self.lboundEdit21.text()]
+            else:
+                lowerbound = sympy.sympify(self.lboundEdit21.text()) if self.lboundEdit21.text().strip() else ''
+
+            f_text = self.funcEdit2.toPlainText().replace('e^(','exp(')  #这里的目的是把'e^'全部用'exp'代替，否则可能有些无穷积分无法得到解析解
+            while f_text.find('e^')!=-1:
+                index = f_text.find('e^') + 2
+                while f_text[index] not in ['+','-','*','/','^',')']:
+                    if index+1 == len(f_text):
+                        break
+                    index += 1
+                f_text = f_text[:index+1] + ')' + f_text[index+1:]
+                f_text = f_text.replace('e^','exp(')
+            f = sympy.sympify(f_text) if self.funcEdit2.toPlainText().strip() else ''
+
+            if self.uboundEdit22.text() in inf:
+                upperbound2 = inf[self.uboundEdit22.text()]
+            else:
+                upperbound2 = sympy.sympify(self.uboundEdit22.text()) if self.uboundEdit22.text().strip() else ''
+            if self.lboundEdit22.text() in inf:
+                lowerbound2 = inf[self.lboundEdit22.text()]
+            else:
+                lowerbound2 = sympy.sympify(self.lboundEdit22.text()) if self.lboundEdit22.text().strip() else ''
+
             self.latex_code = r'\iint_{'+sympy.latex(lowerbound)+'\leq x\leq '+sympy.latex(upperbound)+';'+sympy.latex(lowerbound2)+'\leq y\leq '+sympy.latex(upperbound2)+'}'+sympy.latex(f)+'\mathrm{d}x\mathrm{d}y'
             x = sympy.Symbol('x')
             y = sympy.Symbol('y')
             self.svgWidget2.close()
             self.ans=sympy.integrate(f, (x, lowerbound, upperbound),(y, lowerbound2, upperbound2)) if exchange else sympy.integrate(f, (y, lowerbound2, upperbound2),(x, lowerbound, upperbound))
         else:
-            upperbound = sympy.sympify(self.uboundEdit31.text()) if self.uboundEdit31.text().strip() else ''
-            lowerbound = sympy.sympify(self.lboundEdit31.text()) if self.lboundEdit31.text().strip() else ''
-            f = sympy.sympify(self.funcEdit3.toPlainText()) if self.funcEdit3.toPlainText().strip() else ''
-            upperbound2 = sympy.sympify(self.uboundEdit32.text()) if self.uboundEdit32.text().strip() else ''
-            lowerbound2 = sympy.sympify(self.lboundEdit32.text()) if self.lboundEdit32.text().strip() else ''
-            upperbound3 = sympy.sympify(self.uboundEdit33.text()) if self.uboundEdit33.text().strip() else ''
-            lowerbound3 = sympy.sympify(self.lboundEdit33.text()) if self.lboundEdit33.text().strip() else ''
+            if self.uboundEdit31.text() in inf:
+                upperbound = inf[self.uboundEdit31.text()]
+            else:
+                upperbound = sympy.sympify(self.uboundEdit31.text()) if self.uboundEdit31.text().strip() else ''
+            if self.lboundEdit31.text() in inf:
+                lowerbound = inf[self.lboundEdit31.text()]
+            else:
+                lowerbound = sympy.sympify(self.lboundEdit31.text()) if self.lboundEdit31.text().strip() else ''
+
+            f_text = self.funcEdit3.toPlainText().replace('e^(','exp(')  #这里的目的是把'e^'全部用'exp'代替，否则可能有些无穷积分无法得到解析解
+            while f_text.find('e^')!=-1:
+                index = f_text.find('e^') + 2
+                while f_text[index] not in ['+','-','*','/','^',')']:
+                    if index+1 == len(f_text):
+                        break
+                    index += 1
+                f_text = f_text[:index+1] + ')' + f_text[index+1:]
+                f_text = f_text.replace('e^','exp(')
+            f = sympy.sympify(f_text) if self.funcEdit3.toPlainText().strip() else ''
+
+            if self.uboundEdit32.text() in inf:
+                upperbound2 = inf[self.uboundEdit32.text()]
+            else:
+                upperbound2 = sympy.sympify(self.uboundEdit32.text()) if self.uboundEdit32.text().strip() else ''
+            if self.lboundEdit32.text() in inf:
+                lowerbound2 = inf[self.lboundEdit32.text()]
+            else:
+                lowerbound2 = sympy.sympify(self.lboundEdit32.text()) if self.lboundEdit32.text().strip() else ''
+
+            if self.uboundEdit33.text() in inf:
+                upperbound3 = inf[self.uboundEdit33.text()]
+            else:
+                upperbound3 = sympy.sympify(self.uboundEdit33.text()) if self.uboundEdit33.text().strip() else ''
+            if self.lboundEdit33.text() in inf:
+                lowerbound3 = inf[self.lboundEdit33.text()]
+            else:
+                lowerbound3 = sympy.sympify(self.lboundEdit33.text()) if self.lboundEdit33.text().strip() else ''
+
             self.latex_code = r'\iiint_{'+sympy.latex(lowerbound)+'\leq x\leq '+sympy.latex(upperbound)+';'+sympy.latex(lowerbound2)+'\leq y\leq '+sympy.latex(upperbound2)+';'+sympy.latex(lowerbound3)+'\leq z\leq '+sympy.latex(upperbound3)+'}'+sympy.latex(f)+'\mathrm{d}x\mathrm{d}y\mathrm{d}z'
             x = sympy.Symbol('x')
             y = sympy.Symbol('y')
@@ -268,108 +383,3 @@ class IntFrame(QMainWindow):
                     self.ans=sympy.integrate(f,(x, lowerbound, upperbound),(z, lowerbound3, upperbound3), (y, lowerbound2, upperbound2))
         self.Signal.emit('1')
         self.close()
-        # print(self.ans)
-
-
-
-'''
-constants = {'e':sympy.E, 'pi':sympy.pi}
-op = ['(', ')', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '+', '=','^']
-function_handle = {'':lambda x:x, 'arccos':lambda x:sympy.acos(x), 'arcsin':lambda x:sympy.asin(x), 'arctan':lambda x:sympy.atan(x), 'sin':lambda x:sympy.sin(x), 'cos':lambda x:sympy.cos(x), 'tan':lambda x:sympy.tan(x), 'lg':lambda x:sympy.log(x,10), 'ln':lambda x:sympy.log(x), 'sqrt()':lambda x:sympy.sqrt(x), 'x!':lambda x:sympy.factorial(x), '|x|':lambda x:sympy.Abs(x), 'exp':lambda x:sympy.exp(x)}
-functions = ['sin', 'cos', 'tan', 'lg', 'ln', 'sqrt', 'fac', 'arcsin', 'arccos', 'arctan', 'abs', 'exp']
-operators = ['arcsin', 'arccos', 'sin', 'cos', 'tan', 'arctan', 'lg', 'ln', '(', ')', 'exp', '^', '/', 'sqrt', '*', '-', 'abs', '+']
-#将一个含变量的字符串转译成sympy表达式
-def str2sym(List):
-    CAL=[]
-    FUNC=[]
-    EXP=expression()
-    CAL.append(EXP)
-    for sender in List:
-        if sender in constants.keys():   #按下的是pi或e等常量
-            CAL[-1].curr_num=constants[sender]
-
-        elif sender in functions:   #遇到函数
-            temp=expression()           #新开一个expression对象压入CAL栈
-            CAL.append(temp)            
-            FUNC.append(sender)         #将函数记录到函数栈里面
-
-        elif sender in op:  #如果按下的是运算符
-            if sender=="(":    #新开一个expression类压入CAL栈,同时函数栈压入空字符串
-                temp=expression()
-                CAL.append(temp)
-                FUNC.append("")
-
-            elif sender[0].isdigit(): #如果是数字，直接读成浮点数
-                CAL[-1].curr_num=float(sender) if '.' in sender else int(sender)
-
-            elif (sender=="+" or sender=="-" or sender==")" or sender=="="):    #遇到+-)=，进行计算
-                if (CAL[-1].base_num!=None):                                #结算^运算
-                    CAL[-1].curr_num=CAL[-1].base_num**CAL[-1].curr_num
-                    CAL[-1].base_num=None
-
-                sgn=-1 if (CAL[-1].prev_sym=="-") else 1
-
-                if (CAL[-1].curr_sym==""):                                  #再结算*/运算
-                    CAL[-1].res+=CAL[-1].curr_num*sgn
-
-                elif (CAL[-1].curr_sym=="*"):
-                    CAL[-1].res=CAL[-1].res+sgn*CAL[-1].curr_num*CAL[-1].prev_num
-
-                elif (CAL[-1].curr_sym=="/"):
-                    CAL[-1].res=CAL[-1].res+sgn*CAL[-1].prev_num/CAL[-1].curr_num
-
-                CAL[-1].prev_sym=sender
-                CAL[-1].curr_sym=""
-                CAL[-1].prev_num=CAL[-1].curr_num=None
-
-                if sender ==")":
-                    temp=CAL[-1].res
-                    CAL.pop()
-                    CAL[-1].curr_num=function_handle[FUNC[-1]](temp)
-                    FUNC.pop()
-
-            elif (sender=="*" or sender=="/"):  #遇到*/，记录在prev_sym和prev_num中                
-                if (CAL[-1].base_num!=None):                                #结算^运算
-                    CAL[-1].curr_num=CAL[-1].base_num**CAL[-1].curr_num
-                    CAL[-1].base_num=None
-
-                if CAL[-1].prev_num==None:
-                    CAL[-1].prev_num=CAL[-1].curr_num
-
-                else:
-                    if (CAL[-1].curr_sym=="*"):
-                        CAL[-1].prev_num=CAL[-1].prev_num*CAL[-1].curr_num
-                    elif (CAL[-1].curr_sym=="/"):
-                        CAL[-1].prev_num=CAL[-1].prev_num/CAL[-1].curr_num
-                CAL[-1].curr_sym=sender
-                CAL[-1].curr_num=None
-
-            elif (sender=="^"): #遇到^，
-                if (CAL[-1].base_num!=None):                                #结算^运算
-                    CAL[-1].curr_num=CAL[-1].base_num**CAL[-1].curr_num
-            
-                CAL[-1].base_num=CAL[-1].curr_num
-                CAL[-1].curr_num=None
-
-    if (CAL[-1].base_num!=None):                                #结算^运算
-        CAL[-1].curr_num=CAL[-1].base_num**CAL[-1].curr_num
-        CAL[-1].base_num=None
-
-    sgn=-1 if (CAL[-1].prev_sym=="-") else 1
-
-    if (CAL[-1].curr_sym==""):                                  #再结算*/运算
-        CAL[-1].res+=CAL[-1].curr_num*sgn
-
-    elif (CAL[-1].curr_sym=="*"):
-        CAL[-1].res=CAL[-1].res+sgn*CAL[-1].curr_num*CAL[-1].prev_num
-
-    elif (CAL[-1].curr_sym=="/"):
-        CAL[-1].res=CAL[-1].res+sgn*CAL[-1].prev_num/CAL[-1].curr_num
-
-    CAL[-1].prev_sym=sender
-    CAL[-1].curr_sym=""
-    CAL[-1].prev_num=CAL[-1].curr_num=None
-    # CAL[-1].curr_num_text=""
-
-    return CAL[0].res
-'''
