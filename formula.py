ee69161b8a1a13cac6b3f2e096fefbcd3c454c30
Alpacaaaaaa <97775469+Alpacaaaaaa@users.noreply.github.com>
2022-02-23 22:29:10 +0800

    稍修改 防止轮子造重先搁置

--- formula.py
+++ formula.py
@@ -2,7 +2,9 @@
 
 可以直接在显示区输入等式以快速进行一元方程求解
 
-多元方程施工中
+多元方程这刚看到王神已经很完善了 并且理论上也能实现带符号算
+
+就不重复造轮子了 
 
 '''
 
@@ -18,7 +20,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 class Formula(QMainWindow):
-    Signal = pyqtSignal(str)
+    Signal = pyqtSignal(int)
     def __init__(self, *args, **kwargs):
         
         super().__init__()
@@ -45,39 +47,32 @@ class Formula(QMainWindow):
         self.stackedwidget.addWidget(self.formn)
         
         self.resize(300, 200)
-
-
-    def switch(self, index):
-        if index == 1:
-            self.stackedwidget.setCurrentIndex(0)
-
-        elif index == 2:
-            self.stackedwidget.setCurrentIndex(1)
     
 
     # 界面设置（一元函数情况）
     def setup1(self):
         
         self.Layout1 = QGridLayout(self.form1)
-
+        # 输入ui
         self.unknown_name_label_1 = QLabel()
-        self.unknown_name_label_1.setText('输入未知数名称：')
+        self.unknown_name_label_1.setText('输入未知数名称:')
         self.unknown_name_label_1.setAlignment(Qt.AlignLeft)
         self.unknown_name_1 = QLineEdit()
         self.unknown_name_1.setPlaceholderText('输入未知数(例如:x)')
         self.unknown_name_1.setFocus(True)
-        self.yesbtn = QPushButton('确定')
-        self.yesbtn.clicked.connect(self.solve_1)
-
-        self.yesbtn.setDefault(True)
-        self.yesbtn.setShortcut('Enter')
-        self.nobtn = QPushButton('取消')
-        self.nobtn.clicked.connect(self.close)
-
+        
+        # 动作按钮
+        self.yesbtn_1 = QPushButton('确定')
+        self.yesbtn_1.clicked.connect(self.solve_1)
+        self.yesbtn_1.setDefault(True)
+        self.yesbtn_1.setShortcut('Enter')
+        self.nobtn_1 = QPushButton('取消')
+        self.nobtn_1.clicked.connect(self.close)
+        # 布局
         self.Layout1.addWidget(self.unknown_name_label_1, 1, 0, 1, 2)
         self.Layout1.addWidget(self.unknown_name_1, 2, 0, 1, 2)
-        self.Layout1.addWidget(self.yesbtn, 3, 0, 1, 1)
-        self.Layout1.addWidget(self.nobtn, 3, 1, 1, 1)
+        self.Layout1.addWidget(self.yesbtn_1, 3, 0, 1, 1)
+        self.Layout1.addWidget(self.nobtn_1, 3, 1, 1, 1)
 
     # 界面设置（多元函数情况）
     def setupn(self):
@@ -86,44 +81,157 @@ class Formula(QMainWindow):
 
         self.substack = QStackedWidget()
         
-        self.subform = []
-        
-
+        # 输入ui
         self.count_label = QLabel()
-        self.count_label.setText('输入方程个数:')
+        self.count_label.setText('输入方程个数:(超过3个请使用矩阵运算)')
         self.count_label.setAlignment(Qt.AlignLeft)
-        self.count_input = QLineEdit()
-        self.count_input.setPlaceholderText('输入方程个数')
-        
+        self.count_input = QComboBox()
+        self.count_input.addItems(['1', '2', '3'])
+        self.count_input.currentIndexChanged[int].connect(self.switch)
+      
 
+        self.subform1 = QWidget()
+        self.subset1()
+        self.subform2 = QWidget()
+        self.subset2()
+        self.subform3 = QWidget()
+        self.subset3()
+        self.substack.addWidget(self.subform1)
+        self.substack.addWidget(self.subform2)
+        self.substack.addWidget(self.subform3)
 
-        self.unknown_name_label = QLabel()
-        self.unknown_name_label.setText('输入未知数名称:')
-        self.unknown_name_label.setAlignment(Qt.AlignLeft)
-        self.unknown_name = QLineEdit()
-        self.unknown_name.setPlaceholderText('输入未知数(例如:x)')
+        # 动作按钮
+        self.yesbtn = QPushButton('确定')
+        self.yesbtn.clicked.connect(self.solve_n)
+        self.yesbtn.setDefault(True)
+        self.yesbtn.setShortcut('Enter')
+        self.nobtn = QPushButton('取消')
+        self.nobtn.clicked.connect(self.close)
 
-
+        # 布局
         self.Layoutn.setSpacing(10)
-        self.Layoutn.addWidget(self.count_label)
-        self.Layoutn.addWidget(self.count_input)
-        self.Layoutn.addWidget(self.substack)
-        self.Layoutn.addWidget(self.unknown_name_label)
-        self.Layoutn.addWidget(self.unknown_name)
-        self.Layoutn.addWidget(self.yesbtn)
-        self.Layoutn.addWidget(self.nobtn)
+        self.Layoutn.addWidget(self.count_label, 1, 1, 1, 2)
+        self.Layoutn.addWidget(self.count_input, 2, 1, 1, 2)
+        self.Layoutn.addWidget(self.substack, 3, 1, 1, 2)
+        self.Layoutn.addWidget(self.yesbtn, 6, 1, 1, 1)
+        self.Layoutn.addWidget(self.nobtn, 6, 2, 1, 1)
         
+
+    def subset1(self):
+        self.sublayout1 = QVBoxLayout(self.subform1)
+        self.subformula_label_1_1 = QLabel()
+        self.subformula_label_1_1.setText('请输入方程:')
+        self.subform_input_1_1 = QLineEdit()
+        self.subform_input_1_1.setPlaceholderText('输入完整等式(如x=1)')
+        
+        self.unknown_name_label_1_1 = QLabel()
+        self.unknown_name_label_1_1.setText('输入未知数名称:')
+        self.unknown_name_label_1_1.setAlignment(Qt.AlignLeft)
+        self.unknown_name_1_1 = QLineEdit()
+        self.unknown_name_1_1.setPlaceholderText('(例如:x)')
+        
+        self.sublayout1.addWidget(self.subformula_label_1_1)
+        self.sublayout1.addWidget(self.subform_input_1_1)
+        self.sublayout1.addWidget(self.unknown_name_label_1_1)
+        self.sublayout1.addWidget(self.unknown_name_1_1)
+        self.sublayout1.addStretch(1)
+
+
+    def subset2(self):
+        self.sublayout2 = QVBoxLayout(self.subform2)
+        self.subformula_label_2_1 = QLabel()
+        self.subformula_label_2_1.setText('请输入方程:')
+        self.subform_input_2_1 = QLineEdit()
+        self.subform_input_2_1.setPlaceholderText('输入完整等式(如x=1)')
+        self.subformula_label_2_2 = QLabel()
+        self.subformula_label_2_2.setText('请输入方程:')
+        self.subform_input_2_2 = QLineEdit()
+        self.subform_input_2_2.setPlaceholderText('输入完整等式(如x=1)')
+              
+        self.unknown_name_label_2_1 = QLabel()
+        self.unknown_name_label_2_1.setText('输入未知数1名称:')
+        self.unknown_name_label_2_1.setAlignment(Qt.AlignLeft)
+        self.unknown_name_2_1 = QLineEdit()
+        self.unknown_name_2_1.setPlaceholderText('(例如:x)')
+        self.unknown_name_label_2_2 = QLabel()
+        self.unknown_name_label_2_2.setText('输入未知数2名称:')
+        self.unknown_name_label_2_2.setAlignment(Qt.AlignLeft)
+        self.unknown_name_2_2 = QLineEdit()
+        self.unknown_name_2_2.setPlaceholderText('(例如:x)')
+        
+        self.sublayout2.addWidget(self.subformula_label_2_1)
+        self.sublayout2.addWidget(self.subform_input_2_1)
+        self.sublayout2.addWidget(self.subformula_label_2_2)
+        self.sublayout2.addWidget(self.subform_input_2_2)
+        self.sublayout2.addWidget(self.unknown_name_label_2_1)
+        self.sublayout2.addWidget(self.unknown_name_2_1)
+        self.sublayout2.addWidget(self.unknown_name_label_2_2)
+        self.sublayout2.addWidget(self.unknown_name_2_2)
+        self.sublayout2.addStretch(1)
+
+    
+    def subset3(self):
+        self.sublayout3 = QVBoxLayout(self.subform3)
+        self.subformula_label_3_1 = QLabel()
+        self.subformula_label_3_1.setText('请输入方程:')
+        self.subform_input_3_1 = QLineEdit()
+        self.subform_input_3_1.setPlaceholderText('输入完整等式(如x=1)')
+        self.subformula_label_3_2 = QLabel()
+        self.subformula_label_3_2.setText('请输入方程:')
+        self.subform_input_3_2 = QLineEdit()
+        self.subform_input_3_2.setPlaceholderText('输入完整等式(如x=1)')
+        self.subformula_label_3_3 = QLabel()
+        self.subformula_label_3_3.setText('请输入方程:')
+        self.subform_input_3_3 = QLineEdit()
+        self.subform_input_3_3.setPlaceholderText('输入完整等式(如x=1)')
+        
+        self.unknown_name_label_3_1 = QLabel()
+        self.unknown_name_label_3_1.setText('输入未知数1名称:')
+        self.unknown_name_label_3_1.setAlignment(Qt.AlignLeft)
+        self.unknown_name_3_1 = QLineEdit()
+        self.unknown_name_3_1.setPlaceholderText('(例如:x)')
+        self.unknown_name_label_3_2 = QLabel()
+        self.unknown_name_label_3_2.setText('输入未知数2名称:')
+        self.unknown_name_label_3_2.setAlignment(Qt.AlignLeft)
+        self.unknown_name_3_2 = QLineEdit()
+        self.unknown_name_3_2.setPlaceholderText('(例如:x)')
+        self.unknown_name_label_3_3 = QLabel()
+        self.unknown_name_label_3_3.setText('输入未知数3名称:')
+        self.unknown_name_label_3_3.setAlignment(Qt.AlignLeft)
+        self.unknown_name_3_3 = QLineEdit()
+        self.unknown_name_3_3.setPlaceholderText('(例如:x)')
+        
+        self.sublayout3.addWidget(self.subformula_label_3_1)
+        self.sublayout3.addWidget(self.subform_input_3_1)
+        self.sublayout3.addWidget(self.subformula_label_3_2)
+        self.sublayout3.addWidget(self.subform_input_3_2)
+        self.sublayout3.addWidget(self.subformula_label_3_3)
+        self.sublayout3.addWidget(self.subform_input_3_3)
+        self.sublayout3.addWidget(self.unknown_name_label_3_1)
+        self.sublayout3.addWidget(self.unknown_name_3_1)
+        self.sublayout3.addWidget(self.unknown_name_label_3_2)
+        self.sublayout3.addWidget(self.unknown_name_3_2)
+        self.sublayout3.addWidget(self.unknown_name_label_3_3)
+        self.sublayout3.addWidget(self.unknown_name_3_3)
+
+
+    def switch(self, count):
+        self.count = 0
+        self.substack.setCurrentIndex(count)
+        self.count = count + 1
+    
 
     def input_1(self, exp):
         self.exp = exp
         self.stackedwidget.setCurrentIndex(0)
         self.show()
 
+
     def input_n(self):
         self.stackedwidget.setCurrentIndex(1)
-        self.count = self.count_input.text()
         self.show()
         return
+
 
     def solve_1(self):
         
@@ -145,21 +253,22 @@ class Formula(QMainWindow):
         except:
             self.formula_error()
             
-        self.Signal.emit('1')
+        self.Signal.emit(1)
         self.close()
-
     
-    
-    def formula_error(self):
-        reply = QMessageBox.warning(self, "Warning", "不合法的输入！", QMessageBox.Ok)
-        if (reply == QMessageBox.Ok):
-            return None
-
 
     def solve_n(self):
-        self.count = int(self.count)
+
         return None
 
     
     def output(self):
-        return self.ans
+        return self.ans 
+
+
+    # 异常处理函数
+    def formula_error(self):
+        reply = QMessageBox.warning(self, "Warning", "不合法的输入！", QMessageBox.Ok)
+        self.ans = ''
+        if (reply == QMessageBox.Ok):
+            return None
