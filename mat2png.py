e418ec7e2856b8739d4218225243182be8cbb8ae
wang-hr20 <wang_hr20@163.com>
2022-03-04 21:28:03 +0800

    输入矩阵latex表达式，渲染为latex并以png形式保存

---
+++ mat2png.py
@@ -0,0 +1,25 @@
+'''
+本文件是使用matplotlib以latex渲染矩阵生成png图片的实现。
+！！！重要问题：太慢了，画个png出来大概要两三秒，如果画成svg的话更慢（时间翻倍）
+寻求更好的办法
+'''
+
+import os
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+plt.rc('mathtext', fontset='cm')
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.size'] = 4
+
+#将tex公式（最好是矩阵）渲染成png格式图片.入口参数：string formula为tex公式代码，size为行与列维度（总的，如果是多个矩阵就是各矩阵行数和与列数和）
+def mat2png(formula, size = None, name = 'ans'):
+    # formula = None
+    w,h =  (3,3) if size is None else size
+    fig = plt.figure(figsize=(w*0.15,h*0.1), facecolor=(250/255, 250/255, 250/255))
+    fig.text(0, 0.5, formula)
+    path = name + '.png'  # 文件路径
+    if os.path.exists(path):  # 如果文件存在
+        os.remove(path)  
+    fig.savefig('ans.png', dpi = 400)
+if __name__ == '__main__':
+    mat2png(r'$$\left[ \matrix{ 12 & 5 & 2 \cr 20 & 4 & 8 \cr 2 & 4 & 3 \cr 7 & 1 & 10 \cr} \right]$$',(4,3))
