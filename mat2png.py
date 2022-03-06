a14bbc1f29d85425c15b651e4e8a18b4b7f5a7ff
wang-hr20 <wang_hr20@163.com>
2022-03-06 19:54:39 +0800

    针对矩阵分解，优化图像尺寸估计

--- mat2png.py
+++ mat2png.py
@@ -2,6 +2,7 @@
 本文件是使用matplotlib以latex渲染矩阵生成png图片的实现。
 ！！！重要问题：太慢了，画个png出来大概要两三秒，如果画成svg的话更慢（时间翻倍）
 寻求更好的办法
+更新：优化图片尺寸估计参数
 '''
 
 import os
@@ -9,12 +10,15 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 plt.rc('mathtext', fontset='cm')
 mpl.rcParams['text.usetex'] = True
-mpl.rcParams['font.size'] = 4
 
 #将tex公式（最好是矩阵）渲染成png格式图片.入口参数：string formula为tex公式代码，size为行与列维度（总的，如果是多个矩阵就是各矩阵行数和与列数和）
 def mat2png(formula, size = None, name = 'ans'):
     # formula = None
     w,h =  (3,3) if size is None else size
+    w += formula.count('matrix')
+    mpl.rcParams['font.size'] = 4-formula.count('matrix')*0.5
+    print(formula.count('matrix'))
+    print(formula)
     fig = plt.figure(figsize=(w*0.15,h*0.1), facecolor=(250/255, 250/255, 250/255))
     fig.text(0, 0.5, formula)
     path = name + '.png'  # 文件路径
