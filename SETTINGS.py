51336f0b5447a3d23b3bf9065b80b957261d3628
wang-hr20 <wang_hr20@163.com>
2022-03-10 23:03:42 +0800

    增加有效位数选择

--- SETTINGS.py
+++ SETTINGS.py
@@ -2,7 +2,7 @@
 本文件中定义SETTINGS类，是calculator界面"设置"选项卡弹窗的实现
 '''
 import sys
-from PyQt5.QtWidgets import QWidget, QRadioButton, QButtonGroup, QGridLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QRadioButton, QButtonGroup, QGridLayout, QLabel, QSpinBox,QPushButton
 from PyQt5.QtGui import QColor, QPalette
 class SETTINGS(QWidget):
     def __init__(self):
@@ -25,7 +25,6 @@ class SETTINGS(QWidget):
         grid.addWidget(self.OUTPUT_FORMAT,0,0)
         grid.addWidget(self.math_output,0,1)
         grid.addWidget(self.digit_output,0,2)
-        self.digit_output.toggled.connect(self.output_toggled)
 
         self.ANGLE=QLabel("角度单位：")
         self.rad = QRadioButton('弧度',self)
@@ -37,18 +36,26 @@ class SETTINGS(QWidget):
         grid.addWidget(self.ANGLE,1,0)
         grid.addWidget(self.rad,1,1)
         grid.addWidget(self.deg,1,2)
-        self.rad.toggled.connect(self.angle_toggled)
 
-        # self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('设置')    
+        digit_num = QLabel('有效位数：')
+        self.dig_num = QSpinBox()
+        self.dig_num.setRange(2,8)
+        self.dig_num.setValue(4)
+        self.dig_num.setSingleStep(1)
+        grid.addWidget(digit_num,2,0)
+        grid.addWidget(self.dig_num,2,1)
+        self.setWindowTitle('设置')
 
         self.sym=False
         self.ang="rad"
-    
-    def output_toggled(self):
-            self.sym=False if self.digit_output.isChecked() else True
-            # print(self.sym)
+        self.dig=4
 
-    def angle_toggled(self):
-            self.ang="rad" if self.rad.isChecked() else "deg"
-            # print(self.ang)
+        confirm = QPushButton('确定')
+        confirm.clicked.connect(self.conf)
+        grid.addWidget(confirm,3,2)
+    
+    def conf(self):
+            self.sym = False if self.digit_output.isChecked() else True
+            self.ang = "rad" if self.rad.isChecked() else "deg"
+            self.dig = self.dig_num.value()
+            self.close()
